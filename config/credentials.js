44d8f34f55800509b5cc35e27ca254ada32415b3
Vivek Mourya <rohanmourya879@gmail.com>
2024-05-20 22:14:15 +0530

    first changes

---
+++ config/credentials.js
@@ -0,0 +1,11 @@
+const { mongo } = require("mongoose");
+
+const mongoURI = "mongodb+srv://rohanmourya879:vivek123@cluster0.upxigtg.mongodb.net/Vivek";
+const email = "jayesh007va@gmail.com";
+const password = "rtws hlck rszj qzdv";
+
+module.exports = {
+    mongoURI,
+    email,
+    password
+};
